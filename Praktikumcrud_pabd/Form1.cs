ea019496d4bcd153cf94ac827b18f52bc2a0641c
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-03-21 16:37:47 +0700

    Add project files.

---
+++ Praktikumcrud_pabd/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Praktikumcrud_pabd
+{
+    public partial class Form1: Form
+    {
+        static string connectionString = "Data Source=LAPTOP-PGU1KG1D\\AZKALADZKIA;Initial Catalog=OrganisasiMahasiswa;Integrated Security=True;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
